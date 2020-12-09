1f4d0a3a0f8a09d12be3e550e298a1c9c877eb00
Patryk Obara <dreamer.tan@gmail.com>
2020-12-09 20:49:54 +0100

    Add misc - our internal C++ lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include "config.h"
 
 #include <cstdio>
+#include <string>
 
 #include <SDL.h>
 
+#include "fs_utils.h"
 #include "libs/nuked/opl3.h" // C library test
 
 int main()
 {
-	printf("program version: %s\n", VERSION_STR);
+	printf("program version: %s\n", VERSION);
 
 	// SDL2 test
 	static_assert(SDL_VERSION_ATLEAST(2, 0, 2));
@@ -26,6 +28,9 @@ int main()
 	// C library test
 	opl3_chip chip = {};
 	OPL3_Reset(&chip, 0);
+
+	// code in misc test
+	const std::string home = to_native_path("~");
 
 	return 0;
 }
