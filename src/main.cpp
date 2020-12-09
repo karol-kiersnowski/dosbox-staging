b527d513f92afa1fac4f36f454fe019640219607
Patryk Obara <dreamer.tan@gmail.com>
2020-12-09 11:43:09 +0100

    Add SDL2 dependency to meson example
    - works on Linux
    - not tested on Windows
    - not tested on macOS

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,23 @@
 
 #include <cstdio>
 
+#include <SDL.h>
+
 int main()
 {
-	constexpr auto version = VERSION_STR;
-	printf("version: %s\n", version);
+	printf("program version: %s\n", VERSION_STR);
+
+	static_assert(SDL_VERSION_ATLEAST(2, 0, 2));
+
+	SDL_version compiled;
+	SDL_VERSION(&compiled);
+	printf("SDL (compiled):  %d.%d.%d\n", compiled.major, compiled.minor,
+	       compiled.patch);
+
+	SDL_version linked;
+	SDL_GetVersion(&linked);
+	printf("SDL (linked):    %d.%d.%d\n", linked.major, linked.minor,
+	       linked.patch);
+
 	return 0;
 }
